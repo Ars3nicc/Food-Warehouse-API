d9d974777ae4f358d4b87ee207849e25295603bf
Ars3nicc <franzmanrique21@gmail.com>
2022-04-28 13:35:14 +0800

    initialized model

--- FoodWarehouseAPI/models.py
+++ FoodWarehouseAPI/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.forms import IntegerField
 
 # Create your models here.
+class Warehouse_stock(models.Model):
+    Mfd_id=models.IntegerField(primary_key=True)
+    Name=models.CharField(max_length=50)
+    UnitPrice=models.DecimalField(max_digits=5, decimal_places=2)
+    UnitsInStock=models.PositiveSmallIntegerField()
+    
+    def __str__(self):
+        return self.Name
