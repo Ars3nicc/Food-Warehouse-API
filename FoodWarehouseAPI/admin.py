ba1a5a7451582a8b65e2bea9bf9381621836e244
Ars3nicc <franzmanrique21@gmail.com>
2022-04-28 13:53:12 +0800

    chore: migration

--- FoodWarehouseAPI/admin.py
+++ FoodWarehouseAPI/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from FoodWarehouseAPI.models import warehouse_stocks
+
 # Register your models here.
+admin.site.register(warehouse_stocks)
--- FoodWarehouseAPI/models.py
+++ FoodWarehouseAPI/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.forms import IntegerField
 
 # Create your models here.
-class Warehouse_stock(models.Model):
+class warehouse_stocks(models.Model):
     Mfd_id=models.IntegerField(primary_key=True)
     Name=models.CharField(max_length=50)
     UnitPrice=models.DecimalField(max_digits=5, decimal_places=2)
