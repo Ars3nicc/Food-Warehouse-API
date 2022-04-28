ff04701626c93b5b700ad22442613ba592f2deaf
Ars3nicc <franzmanrique21@gmail.com>
2022-04-28 14:29:25 +0800

    chore: modified urls

--- FoodWarehouseAPI/urls.py
+++ FoodWarehouseAPI/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register('warehouse_stock', views.stockViewSet)
+router.register('warehouse_stocks', views.stockViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
