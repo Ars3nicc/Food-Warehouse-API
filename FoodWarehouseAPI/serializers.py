e5d91b666d180a36e98f6998e46fdcdbf8116f78
Ars3nicc <franzmanrique21@gmail.com>
2022-04-28 14:28:32 +0800

    chore: modified serializers

--- FoodWarehouseAPI/serializers.py
+++ FoodWarehouseAPI/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from . models import warehouse_stock
+from . models import warehouse_stocks
 
 class stockSerializers(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = warehouse_stock
+        model = warehouse_stocks
         fields = ('Mfd_id', 'Name', 'UnitPrice', 'UnitsInStock')
